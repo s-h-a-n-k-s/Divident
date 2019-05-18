571ee6c2c0eb144fd4f8da634da4a41a18d64f3f
s-h-a-n-k-s <ramsdentom@gmail.com>
2019-05-18 18:30:16 +0100

    Add inital project files

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function Header(props) {
+	return (
+		<div className="Header">
+			<FontAwesomeIcon icon="money-bill-wave" className="Image" />
+			<br />
+			<span className="Text">Divident</span>
+		</div>
+	);
+}
