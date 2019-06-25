a61b1c17f996b9630dc8013d63f9433e0eb17ed2
s-h-a-n-k-s <ramsdentom@gmail.com>
2019-06-25 16:37:59 +0100

    Implement Constants; POST request

--- backend/src/main/java/com/nationwide/rest/StockController.java
+++ backend/src/main/java/com/nationwide/rest/StockController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import com.nationwide.business.StockService;
 import com.nationwide.domain.Stock;
+import com.nationwide.constants.Constants;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +20,13 @@ public class StockController {
 	@Autowired
 	private StockService stockService;
 
-	@GetMapping(path = "/stocks")
+	@GetMapping(path = Constants.STOCKS)
 	public ResponseEntity<List<Stock>> getStocks() {
 		return ResponseEntity.ok(stockService.getStocks());
+	}
+
+	@RequestMapping(value = Constants.STOCK, method = RequestMethod.POST)
+	public ResponseEntity<Stock> createStock(@RequestBody Stock stock) {
+		return ResponseEntity.ok(stockService.addStock(stock));
 	}
 }
