32c5d23add8b60d3c8b41bcde9efbf39b7a7ee45
s-h-a-n-k-s <ramsdentom@gmail.com>
2019-07-01 13:00:04 +0100

    Update CORS

--- backend/src/main/java/com/nationwide/rest/StockController.java
+++ backend/src/main/java/com/nationwide/rest/StockController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3001")
+@CrossOrigin(origins = "http://localhost:3000")
 public class StockController {
 
 	@Autowired
