473494023a75284a829b6975ead18faac71dff51
s-h-a-n-k-s <ramsdentom@gmail.com>
2019-07-04 15:21:07 +0100

    Update wording

--- frontend/src/components/ManageStocksEmptyState.js
+++ frontend/src/components/ManageStocksEmptyState.js
@@ -7,7 +7,7 @@ export default function ManageStocksEmptyState(props) {
 	return (
 		<div className="EmptyState">
 			<img src={EmptyStateImage} alt="Empty State" className="EmptyStateImage" />
-			<p className="EmptyStateTitle">You've added no stocks to manage!</p>
+			<p className="EmptyStateTitle">You've got no stocks to manage!</p>
 			<Link to="/search"><button className="CallToAction">Add Stocks</button></Link>
 		</div>
 	);
